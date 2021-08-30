76f0d5ea471d88f4953ace6f8f3f2061070915ea
Gosha <begoshaaaa@gmail.com>
2021-08-30 14:29:46 +0600

    urls for swagger are added

--- source/posts/urls.py
+++ source/posts/urls.py
@@ -4,13 +4,17 @@ from posts.views import (
     comments,
     users
 )
+from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls import url
 
+schema_view = get_swagger_view(title='Blog API')
 urlpatterns = [
-    path('', posts.PostList.as_view()),
-    path('create/', posts.PostCreateView.as_view()),
+    url(r'^$', schema_view),
+    path('post/', posts.PostList.as_view()),
+    path('post/create/', posts.PostCreateView.as_view()),
     path('upvote/', posts.PostUpvoteView.as_view()),
     path('<int:pk>/post', posts.PostDetail.as_view()),
-    path('comments/', comments.CommentList.as_view()),
+    path('comment/', comments.CommentList.as_view()),
     path('comment/create', comments.CommentCreateView.as_view()),
     path('<int:pk>/comment', comments.CommentDetail.as_view()),
     path('users/', users.UserList.as_view()),
